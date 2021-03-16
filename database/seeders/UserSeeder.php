788c56c88cb023535009f094c4b51f89f7150e22
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-03-16 08:32:00 +0700

    Praktikum 2 memasukkan data awal user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
     }
 }
